6e4aced483dd57cad4b23dc5bf05dacdd44db9e9
lipeiyong <lipeiyong@100tal.com>
2020-07-22 10:40:11 +0800

    20200722

--- src/main/java/collections/s3/s3.java
+++ src/main/java/collections/s3/s3.java
@@ -4,16 +4,31 @@ import java.util.HashMap;
 
 public class s3 {
 }
+//class Solution {
+//    public int lengthOfLongestSubstring(String s) {
+//        HashMap<Character , Integer> hashMap = new HashMap<>() ;
+//        int ans = 0 ;
+//        for(int i =  0 ,j = 0 ; j < s.length() ; j++){
+//            if(hashMap.containsKey(s.charAt(j))){
+//                i = Math.max(hashMap.get(s.charAt(j)) , i) ;
+//            }
+//            ans = Math.max(ans , j-i+1);
+//            hashMap.put(s.charAt(j), j+1) ;
+//        }
+//        return ans ;
+//    }
+//}
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashMap<Character , Integer> hashMap = new HashMap<>() ;
+        HashMap<Character , Integer >hashMap = new HashMap<>() ;
+        int left  = 0 ;
         int ans = 0 ;
-        for(int i =  0 ,j = 0 ; j < s.length() ; j++){
-            if(hashMap.containsKey(s.charAt(j))){
-                i = Math.max(hashMap.get(s.charAt(j)) , i) ;
+        for(int i = 0 ; i < s.length() ; i++){
+            if (hashMap.containsKey(s.charAt(i))) {
+                left = Math.max(left, hashMap.get(s.charAt(i)) +1 );
             }
-            ans = Math.max(ans , j-i+1);
-            hashMap.put(s.charAt(j), j+1) ;
+            hashMap.put(s.charAt(i) , i )  ;
+            ans  = Math.max(ans , i - left +1 ) ;
         }
         return ans ;
     }
---
+++ src/main/java/collections/s7/s7.java
@@ -0,0 +1,9 @@
+package collections.s7;
+
+public class s7 {
+}
+class Solution {
+    public int reverse(int x) {
+
+    }
+}
